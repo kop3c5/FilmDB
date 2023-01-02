70df8181c23bff0a1a2fde2771bc6804b1f25cb3
kop3c <kop3c5@gmail.com>
2023-01-02 12:53:45 +0100

    new classes film manager

---
+++ FilmDB/FilmDB/Controllers/FilmController.cs
@@ -0,0 +1,28 @@
+﻿using FilmDB.Logic;
+using FilmDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB.Controllers
+{
+    public class FilmController : Controller
+    {
+        FilmManager filmManager = new FilmManager();
+        
+        public IActionResult Index()
+        {
+            var random = new Random();
+            var film = new FilmModel()
+            {
+
+                Title = $"Rambo{random.Next(1, 100)}",
+                Year = 1999
+            };
+            filmManager.AddFilm(film);
+            return View();
+        }
+    }
+}
